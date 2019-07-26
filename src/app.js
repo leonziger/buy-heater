7ef58ac4f1f7bf3c0be78864523ea827381a80ee
Uma <buldog52@gmail.com>
2019-07-26 13:13:37 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+window.$ = window.jQuery = require('jquery');
+import FlipClock from 'flipclock';
+
+
+import './components/main-header/main-header';
+import './components/main-banner/main-banner';
+import './components/menu-toggle/menu-toggle';
+import './components/modal/modal';
+import './components/callback/callback';
+import './components/scroll-to-anchor/scroll-to-anchor';
+
+
+
+
+const el = document.querySelector('.clock');
+
+const clock = new FlipClock(el, new Date, {
+    face: 'HourCounter'
+});
